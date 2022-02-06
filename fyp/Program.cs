6c8b8e3051883aaa2620c564b9e0e7b080fdb431
Rrraaaeee <qingxuannn0602@gmail.com>
2022-02-06 16:54:08 +0800

    Added file IO to read inbound and outbound shipments from file

--- fyp/Program.cs
+++ fyp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using O2DESNet;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 
 namespace fyp
@@ -12,8 +13,9 @@ namespace fyp
             // construct the model
             var sim = new MySimModel();
 
-            // run 15 events
-            sim.Run(15);
+            // run 1000 events
+            Console.WriteLine("Simulating 1000 events...\n");
+            sim.Run(1000);
             Console.WriteLine("Simulation Successful...");
 
         }
@@ -29,6 +31,8 @@ namespace fyp
 
         public void arrive(InboundShipment shipment)
         {
+            Console.WriteLine("Order arriving...\n");
+            Debug.Assert(shipment != null);
             shipment.inboundTime = ClockTime;
             shipment.listInboundShipments();
 
@@ -43,20 +47,18 @@ namespace fyp
 
         public void order(OutboundOrder order)
         {
-            Console.WriteLine("Picking an order...");
+            Console.WriteLine("Picking an order...\n");
             order.listOutboundOrders();
             storage.pickStorage(order);
         }
 
-        public MySimModel()
+        public void generateTestInputs()
         {
-            storage = new Storage(3,3,3); // Create new storage
-
             // Create three inbound shipments
             var in1 = new InboundShipment();
             var in2 = new InboundShipment();
             var in3 = new InboundShipment();
-            
+
             in1.addShipmentLine(new InboundShipmentLine(new SKU(1), 5, -1))
                .addShipmentLine(new InboundShipmentLine(new SKU(2), 10, -1))
                .addShipmentLine(new InboundShipmentLine(new SKU(3), 2, -1));
@@ -89,13 +91,50 @@ namespace fyp
 
 
             // Schedule 3 arrival events and 3 order events
-            Schedule(() => arrive(in1), new TimeSpan(4*sec));
-            Schedule(() => arrive(in2), new TimeSpan(8*sec));
-            Schedule(() => arrive(in3), new TimeSpan(2*sec));
+            Schedule(() => arrive(in1), new TimeSpan(4 * sec));
+            Schedule(() => arrive(in2), new TimeSpan(8 * sec));
+            Schedule(() => arrive(in3), new TimeSpan(2 * sec));
 
             Schedule(() => order(out1), new TimeSpan(5 * sec));
             Schedule(() => order(out2), new TimeSpan(9 * sec));
             Schedule(() => order(out3), new TimeSpan(3 * sec));
+
+        }
+
+        public MySimModel()
+        {
+            storage = new Storage(3,3,3); // Create new storage
+
+            var io = new fileIO();
+
+            // register inbound
+            while(true)
+            {
+                var (time, inbound) = io.getNextInbound();
+                if (inbound == null)
+                {
+                    break;
+                }
+                //Console.WriteLine(time)
+                Schedule(() => arrive(inbound), time);
+            };
+
+            // register outbound
+            while (true)
+            {
+                var (time, outbound) = io.getNextOutbound();
+                if (outbound == null)
+                {
+                    break;
+                }
+                //Console.WriteLine(time)
+                Schedule(() => order(outbound), time);
+            };
+
+
+
+
+            //io.getNextOutbound();
         }
     }
 }
---
+++ fyp/fileIO.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+
+namespace fyp
+{
+    internal class fileIO
+    {
+        public fileIO()
+        {
+            String curPath = Environment.CurrentDirectory;
+            String basePath = Directory.GetParent(curPath).Parent.Parent.Parent.FullName;
+            String inboundFile = Path.Join(basePath, "Inbound.csv");
+            String outboundFile = Path.Join(basePath, "Outbound.csv");
+            inboundReader = new StreamReader(inboundFile);
+            outboundReader = new StreamReader(inboundFile);
+            inboundReader.ReadLine(); // skip header
+            outboundReader.ReadLine(); // skip header
+            inboundBufferedLine = null;
+            outboundBufferedLine = null;
+
+        }
+
+        ~fileIO()
+        {
+            inboundReader.Close();
+            outboundReader.Close();
+        }
+
+        public StreamReader inboundReader;
+        public StreamReader outboundReader;
+
+        private String inboundBufferedLine;
+        private String outboundBufferedLine;
+
+        // TODO: Make sure for the same shipmentNO, time is the same
+        public (TimeSpan, InboundShipment) getNextInbound()
+        {            
+            if (inboundReader.EndOfStream && inboundBufferedLine == null)
+            {
+                // no more input
+                return (TimeSpan.MinValue, null);
+            }
+
+            var shipment = new InboundShipment();
+            int lastShipmentNo = -1;
+            TimeSpan shipmentTime = TimeSpan.MinValue;
+
+            do
+            {
+                // when inboundBufferedLine != null,
+                // that means we have peeked the last input
+                if (inboundBufferedLine == null)
+                {
+                    inboundBufferedLine = inboundReader.ReadLine();
+                }
+
+                //Console.WriteLine(inboundBufferedLine);
+                var fields = inboundBufferedLine.Split(",");
+                var shipmentNo = int.Parse(fields[0]);
+                var time = TimeSpan.Parse(fields[1]);
+                var orderNo = int.Parse(fields[2]);
+                var sku = int.Parse(fields[3]);
+                var qty = int.Parse(fields[4]);
+
+                if (lastShipmentNo!=-1 && lastShipmentNo != shipmentNo)
+                {
+                    // next line is a new shipment
+                    break;
+                }
+
+                shipment.addShipmentLine(new InboundShipmentLine(new SKU(sku), qty, orderNo));
+
+                shipmentTime = time;
+                lastShipmentNo = shipmentNo;
+                inboundBufferedLine = null;
+
+            } while (!inboundReader.EndOfStream);
+
+            return (shipmentTime, shipment);
+        }
+
+        // TODO: Make sure for the same shipmentNO, time is the same
+        public (TimeSpan, OutboundOrder) getNextOutbound()
+        {
+            if (outboundReader.EndOfStream && outboundBufferedLine == null)
+            {
+                // no more input
+                return (TimeSpan.MinValue, null);
+            }
+
+            var shipment = new OutboundOrder();
+            int lastShipmentNo = -1;
+            TimeSpan shipmentTime = TimeSpan.MinValue;
+
+            do
+            {
+                // when outboundBufferedLine != null,
+                // that means we have peeked the last input
+                if (outboundBufferedLine == null)
+                {
+                    outboundBufferedLine = outboundReader.ReadLine();
+                }
+
+                //Console.WriteLine(inboundBufferedLine);
+                var fields = outboundBufferedLine.Split(",");
+                var shipmentNo = int.Parse(fields[0]);
+                var time = TimeSpan.Parse(fields[1]);
+                var orderNo = int.Parse(fields[2]);
+                var sku = int.Parse(fields[3]);
+                var qty = int.Parse(fields[4]);
+
+                if (lastShipmentNo != -1 && lastShipmentNo != shipmentNo)
+                {
+                    // next line is a new shipment
+                    break;
+                }
+
+                shipment.addOrderLine(new OutboundOrderLine(new SKU(sku), qty, orderNo));
+
+                shipmentTime = time;
+                lastShipmentNo = shipmentNo;
+                outboundBufferedLine = null;
+
+            } while (!outboundReader.EndOfStream);
+
+            return (shipmentTime, shipment);
+        }
+
+    }
+}
